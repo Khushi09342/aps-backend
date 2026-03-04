a5ded97073106ac732a2816feee71210f197317e
Khushi Patil <khushi.patil@desapex.com>
2026-03-04 11:47:54 +0530

    fix server routes

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 7071;
+const { spawn } = require("child_process");
 
-require("./src/functions/reviews");
-require("./src/functions/forms");
-require("./src/functions/login");
-require("./src/functions/callback");
+const app = express();
+const port = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
   res.send("APS Backend Running");
+});
+
+app.get("/api/reviews", (req, res) => {
+  res.send("Reviews endpoint running");
+});
+
+app.get("/api/forms", (req, res) => {
+  res.send("Forms endpoint running");
 });
 
 app.listen(port, () => {
